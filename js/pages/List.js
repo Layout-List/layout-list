a5bfef1a07532aba51bde9b142f48cf0c0daaec5
BlathersB <110778021+BlathersB@users.noreply.github.com>
2022-09-12 20:32:24 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -76,7 +76,7 @@ export default {
                                 <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
                             </td>
                             <td class="hz">
-                                <p>{{ record.hz }}Hz</p>
+                                <p>{{ record.hz }}<!--Hz--></p>
                             </td>
                         </tr>
                     </table>
