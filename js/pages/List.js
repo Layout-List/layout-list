b9ca4792d77c7939ba9ffe35d6f0d1debef691d4
BlathersB <110778021+BlathersB@users.noreply.github.com>
2022-09-12 20:36:45 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -62,7 +62,7 @@ export default {
                         </li>
                     </ul>
                     <h2>Records</h2>
-                    <p v-if="selected + 1 <= 150"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-if="selected + 1 <= 150"><strong>{{ level.percentToQualify }}%</strong> or better to qualify                                   FPS</p>
                     <p v-else>This level does not accept new records.</p>
                     <table class="records">
                         <tr v-for="record in level.records" class="record">
