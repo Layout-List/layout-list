6b9d23f810d87b7dfb27c95ea93d02b51fe2e501
DJJDK <69492237+DJJDK@users.noreply.github.com>
2023-07-08 13:15:28 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,7 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import ListPacks from './pages/ListPacks.js'
 
 export default [
     { path: '/', component: List },
