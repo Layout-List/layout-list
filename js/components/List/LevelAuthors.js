7a1ed1c16972d797eb7f4af04dec3633a96f47a8
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-02-06 13:37:16 -0500

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -35,6 +35,77 @@ export default {
                     <span>{{ verifier }}</span>
                 </p>
             </template>
+	        <template v-else-if=”hosts.length === 1”>
+	            <div class=”type-title-sm”>Host</div>
+	            <p class=”type-body”>
+	                <span>{{ host }}</span>
+	            </p>
+                <div class="type-title-sm">Creators</div>
+                <p class="type-body">
+                    <template v-for="(creator, index) in [author, ...creators]" :key="\`creator-\$\{creator\}\`">
+                        <span>{{ creator }}</span>
+                        <span v-if="index < creators.length">, </span>
+                    </template>
+                </p>
+                <div class="type-title-sm">Verifier</div>
+                <p class="type-body">
+                    <span>{{ verifier }}</span>
+                </p>
+            </template>
+	        <template v-else-if=”hosts.length > 1”>
+	            <div class=”type-title-sm”>Hosts</div>
+	            <p class="type-body">
+                    <template v-for="(host, index) in [hosts]" :key="\`host-\$\{host\}\`">
+                        <span>{{ host }}</span>
+                        <span v-if="index < host.length">, </span>
+                    </template>
+                </p>
+                <div class="type-title-sm">Creators</div>
+                <p class="type-body">
+                    <template v-for="(creator, index) in [author, ...creators]" :key="\`creator-\$\{creator\}\`">
+                        <span>{{ creator }}</span>
+                        <span v-if="index < creators.length">, </span>
+                    </template>
+                </p>
+                <div class="type-title-sm">Verifier</div>
+                <p class="type-body">
+                    <span>{{ verifier }}</span>
+                </p>
+            </template>
+	        <template v-else>
+	            <div class="type-title-sm">Creators</div>
+                <p class="type-body">
+                    <template v-for="(creator, index) in [author, ...creators]" :key="\`creator-\$\{creator\}\`">
+                        <span>{{ creator }}</span>
+                        <span v-if="index < creators.length">, </span>
+                    </template>
+                </p>
+                <div class="type-title-sm">Verifier</div>
+                <p class="type-body">
+                    <span>{{ verifier }}</span>
+                </p>
+            </template>
+        </div>
+    `,
+
+    /* template: `
+        <div class="level-authors">
+            <template v-if="selfVerified">
+                <div class="type-title-sm">Creator & Verifier</div>
+                <p class="type-body">
+                    <span>{{ author }}</span>
+                </p>
+            </template>
+            <template v-else-if="creators.length === 0">
+                <div class="type-title-sm">Creator</div>
+                <p class="type-body">
+                    <span>{{ author }}</span>
+                </p>
+                <div class="type-title-sm">Verifier</div>
+                <p class="type-body">
+                    <span>{{ verifier }}</span>
+                </p>
+            </template>
             <template v-else>
                 <div class="type-title-sm">Creators</div>
                 <p class="type-body">
@@ -49,7 +120,7 @@ export default {
                 </p>
             </template>
         </div>
-    `,
+    `, */
 
     computed: {
         selfVerified() {
