fcd258fbb684bffb25b4e8846448e1eb162566c0
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-02-13 14:38:17 -0500

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -90,7 +90,7 @@ export default {
                                     <p v-else>#{{ score.rank }}</p>
                                 </td>
                                 <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }} {{ score.percent }}%</a>
+                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }} - {{ score.percent }}%</a>
                                 </td>
                                 <td class="score">
                                     <p>+{{ localize(score.score) }}</p>
