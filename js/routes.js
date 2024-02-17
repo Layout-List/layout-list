6fac0c43ec096754ead467673492f67376626326
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-02-17 15:55:13 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,10 +1,12 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
+import CreatorLeaderboard from './pages/CreatorLeaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
+    { path: '/creatorleaderboard', component: CreatorLeaderboard },
     { path: '/roulette', component: Roulette },
 ];
 // import CreatorLeaderboard from './pages/CreatorLeaderboard.js';
