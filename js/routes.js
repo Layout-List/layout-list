847a62583d366a7108a112df079a5c0d4886d69d
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-02-17 15:45:26 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,10 +1,10 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
+import Cleaderboard from './pages/Cleaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
-    { path: '/creatorleaderboard', component: Cleaderboard },
     { path: '/roulette', component: Roulette },
 ];
