2aed0804ecbfba636f24edb3d896b6f78aeac1fa
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-02-17 16:15:30 -0500

    Update CreatorLeaderboard.js

--- js/pages/CreatorLeaderboard.js
+++ js/pages/CreatorLeaderboard.js
@@ -1,4 +1,125 @@
-import { fetchCreatorLeaderboard } from '../creatorcontent.js';
+import { fetchLeaderboard } from '../content.js';
+import { localize } from '../util.js';
+
+import Spinner from '../components/Spinner.js';
+
+export default {
+    components: {
+        Spinner,
+    },
+    data: () => ({
+        leaderboard: [],
+        loading: true,
+        selected: 0,
+        err: [],
+    }),
+    template: `
+        <main v-if="loading">
+            <Spinner></Spinner>
+        </main>
+        <main v-else class="page-leaderboard-container">
+            <div v-if="!leaderboard" class="page-leaderboard">
+                <div class="error-container">
+                    <p class="error" v-if="err.length > 0">
+                        {{ err[0] }}
+                    </p>
+                </div>
+            </div>
+            <div v-else class="page-leaderboard">
+                <div class="error-container">
+                    <p class="error" v-if="err.length > 0">
+                        Leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
+                    </p>
+                </div>
+                <div class="board-container">
+                    <table class="board">
+                        <tr v-for="(ientry, i) in leaderboard">
+                            <td class="rank">
+                                <p class="type-label-lg">#{{ i + 1 }}</p>
+                            </td>
+                            <td class="total">
+                                <p class="type-label-lg">{{ localize(ientry.total) }}</p>
+                            </td>
+                            <td class="user" :class="{ 'active': selected == i }">
+                                <button @click="selected = i">
+                                    <span class="type-label-lg">{{ ientry.user }}</span>
+                                </button>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="player-container">
+                    <div class="player">
+                        <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
+                        <h4>{{ localize(entry.total) + " / " + localize(entry.possibleMax) }}</h4>
+                        <h2 v-if="entry.verified.length > 0">Verified</h2>
+                        <table class="table">
+                            <tr v-for="score in entry.verified">
+                                <td class="rank">
+                                    <p v-if="score.rank === null">&mdash;</p>
+                                    <p v-else>#{{ score.rank }}</p>
+                                </td>
+                                <td class="level">
+                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
+                                </td>
+                                <td class="score">
+                                    <p>+{{ localize(score.score) }}</p>
+                                </td>
+                            </tr>
+                        </table>
+                        <h2 v-if="entry.completed.length > 0">Completed</h2>
+                        <table class="table">
+                            <tr v-for="score in entry.completed">
+                                <td class="rank">
+                                    <p v-if="score.rank === null">&mdash;</p>
+                                    <p v-else>#{{ score.rank }}</p>
+                                </td>
+                                <td class="level">
+                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
+                                </td>
+                                <td class="score">
+                                    <p>+{{ localize(score.score) }}</p>
+                                </td>
+                            </tr>
+                        </table>
+                        <h2 v-if="entry.progressed.length > 0">Progressed</h2>
+                        <table class="table">
+                            <tr v-for="score in entry.progressed">
+                                <td class="rank">
+                                    <p v-if="score.rank === null">&mdash;</p>
+                                    <p v-else>#{{ score.rank }}</p>
+                                </td>
+                                <td class="level">
+                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }} - {{ score.percent }}%</a>
+                                </td>
+                                <td class="score">
+                                    <p>+{{ localize(score.score) }}</p>
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </main>
+    `,
+    computed: {
+        entry() {
+            return this.leaderboard[this.selected];
+        },
+    },
+    async mounted() {
+        const [leaderboard, err] = await fetchLeaderboard();
+        this.leaderboard = leaderboard;
+        this.err = err;
+        // Hide loading spinner
+        this.loading = false;
+    },
+    methods: {
+        localize,
+    },
+};
+
+/* import { fetchCreatorLeaderboard } from '../creatorcontent.js';
 
 import Spinner from '../components/Spinner.js';
 
@@ -27,7 +148,7 @@ export default {
             <div v-else class="page-leaderboard">
                 <div class="error-container">
                     <p class="error" v-if="err.length > 0">
-                        Creator Leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
+                        Creator leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
                     </p>
                 </div>
                 <div class="board-container">
@@ -109,127 +230,6 @@ export default {
     async mounted() {
         const [cLeaderboard, err] = await fetchCreatorLeaderboard();
         this.cLeaderboard = cLeaderboard;
-        this.err = err;
-        // Hide loading spinner
-        this.loading = false;
-    },
-    methods: {
-        localize,
-    },
-};
-
-/* import { fetchcLeaderboard } from '../creatorcontent.js';
-import { localize } from '../util.js';
-
-import Spinner from '../components/Spinner.js';
-
-export default {
-    components: {
-        Spinner,
-    },
-    data: () => ({
-        cLeaderboard: [],
-        loading: true,
-        selected: 0,
-        err: [],
-    }),
-    template: `
-        <main v-if="loading">
-            <Spinner></Spinner>
-        </main>
-        <main v-else class="page-creatorleaderboard-container">
-            <div v-if="!creatorleaderboard" class="page-creatorleaderboard">
-                <div class="error-container">
-                    <p class="error" v-if="err.length > 0">
-                        {{ err[0] }}
-                    </p>
-                </div>
-            </div>
-            <div v-else class="page-creatorleaderboard">
-                <div class="error-container">
-                    <p class="error" v-if="err.length > 0">
-                        Creator leaderboard may be incorrect, as the following levels could not be loaded: {{ err.join(', ') }}
-                    </p>
-                </div>
-                <div class="board-container">
-                    <table class="board">
-                        <tr v-for="(ientry, i) in cLeaderboard">
-                            <td class="rank">
-                                <p class="type-label-lg">#{{ i + 1 }}</p>
-                            </td>
-                            <td class="total">
-                                <p class="type-label-lg">{{ localize(ientry.total) }}</p>
-                            </td>
-                            <td class="user" :class="{ 'active': selected == i }">
-                                <button @click="selected = i">
-                                    <span class="type-label-lg">{{ ientry.user }}</span>
-                                </button>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-                <div class="player-container">
-                    <div class="player">
-                        <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
-                        <h4>{{ localize(entry.total)
-                        <h2 v-if="entry.solos.length > 0">Solos</h2>
-                        <table class="table">
-                            <tr v-for="score in entry.solos">
-                                <td class="rank">
-                                    <p v-if="score.rank === null">&mdash;</p>
-                                    <p v-else>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                        <h2 v-if="entry.levelsHosted.length > 0">Levels Hosted</h2>
-                        <table class="table">
-                            <tr v-for="score in entry.levelsHosted">
-                                <td class="rank">
-                                    <p v-if="score.rank === null">&mdash;</p>
-                                    <p v-else>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                        <h2 v-if="entry.collabParts.length > 0">Collab Parts</h2>
-                        <table class="table">
-                            <tr v-for="score in entry.collabParts">
-                                <td class="rank">
-                                    <p v-if="score.rank === null">&mdash;</p>
-                                    <p v-else>#{{ score.rank }}</p>
-                                </td>
-                                <td class="level">
-                                    <a class="type-label-lg" target="_blank" :href="score.link">{{ score.level }}</a>
-                                </td>
-                                <td class="score">
-                                    <p>+{{ localize(score.score) }}</p>
-                                </td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </main>
-    `,
-    computed: {
-        entry() {
-            return this.cLeaderboard[this.selected];
-        },
-    },
-    async mounted() {
-        const [cLeaderboard, err] = await fetchcLeaderboard();
-        this.cLeaderboard = cleaderboard;
         this.err = err;
         // Hide loading spinner
         this.loading = false;
