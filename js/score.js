491a256320af051ca27a92b6b9250dfe6fb0430e
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-02-19 13:30:11 -0500

    Update score.js

--- js/score.js
+++ js/score.js
@@ -111,7 +111,7 @@ export function cscore(contributorrole) {
 
 export function enjoyment(level) {
     let score = 0;
-    for (record in level.records) {
+    level.forEach(record) {
         if (record.enjoyment !== null) {
             score += record.enjoyment;
         }
