aac24df14a9422efaa8ed7c3936b7f119d72c9c2
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-03-13 10:18:06 -0400

    Update LevelEnjoyment.js

--- js/components/List/LevelEnjoyment.js
+++ js/components/List/LevelEnjoyment.js
@@ -8,7 +8,12 @@ export default {
 
     template: `
         <div class="level-enjoyment">
-            <template v-if="records.length === 0">
+            <template v-if="1 === 1">
+                <p class="type-body">
+                    <span>{{ "hi" }}</span>
+                </p>
+            </template>
+            <template v-else-if="records.length === 0">
                 <div class="type-title-sm">Average Enjoyment</div>
                 <p class="type-body">
                     <span>{{ "N/A" }}</span>
