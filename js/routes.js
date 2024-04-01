86ad7a1f655f90873eb9408fa972422c0ef2c8b6
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-03-31 23:53:05 -0400

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -8,5 +8,5 @@ export default [
     { path: '/leaderboard', component: Leaderboard },
     { path: '/creatorleaderboard', component: CreatorLeaderboard },
     { path: '/roulette', component: Roulette },
-    { path: '/fools', component: Fools },
+    { path: '/fools', component: List },
 ];
