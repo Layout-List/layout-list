cece8b1915a6f5876650a3cb2751d0e72174a080
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-04-15 22:06:51 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -65,7 +65,7 @@ export default {
                         </li>
                         <li>
                             <div class="type-title-sm">Enjoyment</div>
-                            <p>{{ level.avgEnjoyment }}</p>
+                            <p>{{ level.enjoyment || "?/10" }}</p>
                         </li>
                     </ul>
                     <ul class="stats">
