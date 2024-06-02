ee72da815e30a1ee2950a2e4fb4d622a9aa8a9a7
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-06-02 10:56:37 -0400

    Update ChallengeList.js

--- js/pages/ChallengeList.js
+++ js/pages/ChallengeList.js
@@ -1,7 +1,7 @@
 import { store } from '../main.js';
 import { embed } from '../util.js';
 import { score } from '../score.js';
-import { fetchEditors, fetchList } from '../content.js';
+import { fetchEditors, fetchChallengeList } from '../content.js';
 
 import Spinner from '../components/Spinner.js';
 import LevelAuthors from '../components/List/LevelAuthors.js';
