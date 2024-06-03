a94592029d0eef6d71bfbbf6ac3c92463feaa506
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-06-03 00:24:07 -0400

    Update ChallengeList.js

--- js/pages/ChallengeList.js
+++ js/pages/ChallengeList.js
@@ -1,6 +1,6 @@
 import { store } from '../main.js';
 import { embed } from '../util.js';
-import { score } from '../score.js';
+import { challengeScore } from '../score.js';
 import { fetchEditors, fetchChallengeList } from '../content.js';
 
 import Spinner from '../components/Spinner.js';
@@ -53,7 +53,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points</div>
-                            <p>{{ score(level.difficulty, 100, level.percentToQualify) }}</p>
+                            <p>{{ challengeScore(level.difficulty) }}</p>
                         </li>
                         <li>
                             <div class="type-title-sm">ID</div>
@@ -254,6 +254,6 @@ export default {
     },
     methods: {
         embed,
-        score
+        challengeScore
     },
 };
