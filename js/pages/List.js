3b74b5a52b436ac49c877e34159fb1f62e2110be
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-06-03 20:36:32 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -32,7 +32,7 @@ export default {
                         </router-link>
                     </div>
                 </nav>
-                <h1> </h1>
+                <h1>‎ </h1>
                 <table class="list" v-if="list">
                     <tr v-for="([err, rank, level], i) in list">
                         <td class="rank">
