83a6908acc7f20a976adc9cde06f8f4592e4ed08
sphericle <86196223+sphericle@users.noreply.github.com>
2024-08-04 20:05:54 -0400

    Update ChallengeLeaderboard.js

--- js/pages/ChallengeLeaderboard.js
+++ js/pages/ChallengeLeaderboard.js
@@ -42,7 +42,7 @@ export default {
                             </router-link>
                         </div>
                     </nav>
-                    <div class="notice" style="margin:22px">
+                    <div class="notice" style="margin-bottom:22px">
                         <p>The challenge list has been archived as of 7/20/24. No new layouts or records will be added.</p>
                     </div>
                     <table class="board">
