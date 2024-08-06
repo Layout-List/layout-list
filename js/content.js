881b1648d2585056456d957860de4f44efcef64d
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-08-06 02:55:17 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -74,27 +74,6 @@ export async function fetchTierMinimum(difficulty) {
     return tierMin;
 }
 
-export async function fetchTierLength(difficulty) {
-    const list = await fetchList();
-    let tierLength = 0;
-    list.forEach(([err, rank, level]) => {
-        if (err) {
-            errs.push(err);
-            return;
-        }
-
-        if (rank === null) {
-            return;
-        }
-
-        if (level.difficulty === difficulty) {
-            tierLength += 1;
-        }
-    });
-
-    return 6;
-}
-
 export async function fetchChallengeList() {
     const challengeListResult = await fetch(`${dir}/_challengeList.json`);
     try {
