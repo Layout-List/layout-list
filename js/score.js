9e2126ef3385d6e4d2c39b86c7dcef3e0c286870
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-08-06 02:54:39 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -15,7 +15,7 @@ const scale = 1;
 export function score(rank, difficulty, percent, minPercent) {
     let score = 0;
     /* const tierMin = fetchTierMinimum(difficulty); */
-    let tierLength = await fetchTierLength(difficulty);
+    let tierLength = fetchTierLength(difficulty);
     
     if (difficulty<4){
         minPercent = 100;
