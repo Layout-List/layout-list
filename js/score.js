158f0e5a6b37da209ce5ebe4f89353265b089b50
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-08-06 03:24:31 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -5,7 +5,6 @@ import { fetchTierMinimum } from './content.js';
  * Numbers of decimal digits to round to
  */
 const scale = 1;
-const list = fetchList();
 
 /**
  * Calculate the score awarded when having a certain percentage on a list level
