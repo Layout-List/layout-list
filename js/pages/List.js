6f9f6471f87ce821e8e1b269ef724ac8eaaa3592
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-08-06 03:30:13 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -63,7 +63,7 @@ export default {
                     <ul class="stats">
                         <li>
                             <div class="type-title-sm">Points</div>
-                            <p>{{ score(1, level.difficulty, 100, level.percentToQualify) }}</p>
+                            <p>{{ score(rank, level.difficulty, 100, level.percentToQualify) }}</p>
                         </li>
                         <li>
                             <div class="type-title-sm">ID</div>
