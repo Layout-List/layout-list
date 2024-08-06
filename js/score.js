5fe69db1bfb60b93a97e84993de4dbea364b3d08
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-08-06 03:13:23 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -243,7 +243,7 @@ export function round(num) {
 }
 
 export function fetchTierLength(difficulty) {
-    /*list = fetchList();*/
+    list = fetchList();
     let a = 4;
     /*list.forEach(([err, rank, level]) => {
         if (err) {
