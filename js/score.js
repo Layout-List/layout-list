103dbb83a2a940a1665b7d388ea05cc3710b8eae
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-08-06 03:11:18 -0400

    Update score.js

--- js/score.js
+++ js/score.js
@@ -244,8 +244,8 @@ export function round(num) {
 
 export function fetchTierLength(difficulty) {
     list = fetchList();
-    let tierLength = 0;
-    list.forEach(([err, rank, level]) => {
+    let tierLength = 4;
+    /*list.forEach(([err, rank, level]) => {
         if (err) {
             errs.push(err);
             return;
@@ -258,7 +258,7 @@ export function fetchTierLength(difficulty) {
         if (level.difficulty === difficulty) {
             tierLength += 1;
         }
-    });
+    }); */
 
-    return 6;
+    return tierLength;
 }
