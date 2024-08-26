f38fa3299713cc21a0e8070b09a982d862775177
sphericle <86196223+sphericle@users.noreply.github.com>
2024-08-25 22:45:26 -0400

    show ratings on leaderboard page (#19)

--- js/content.js
+++ js/content.js
@@ -164,6 +164,7 @@ export async function fetchLeaderboard() {
                     level: level.name,
                     score: score(level.difficulty, 100, level.percentToQualify),
                     link: record.link,
+                    rating: record.enjoyment,
                 });
                 return;
             }
@@ -174,6 +175,7 @@ export async function fetchLeaderboard() {
                 percent: record.percent,
                 score: score(level.difficulty, record.percent, level.percentToQualify),
                 link: record.link,
+                rating: record.enjoyment,
             });
         });
     });
