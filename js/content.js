80cfe159be10072e7a6ed2347183a3ed2cfcfedc
sphericle <86196223+sphericle@users.noreply.github.com>
2024-08-25 22:19:43 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -164,7 +164,7 @@ export async function fetchLeaderboard() {
                     level: level.name,
                     score: score(level.difficulty, 100, level.percentToQualify),
                     link: record.link,
-                    rating: record.enjoyment || "?",
+                    rating: record.enjoyment,
                 });
                 return;
             }
@@ -175,7 +175,7 @@ export async function fetchLeaderboard() {
                 percent: record.percent,
                 score: score(level.difficulty, record.percent, level.percentToQualify),
                 link: record.link,
-                rating: record.enjoyment || "?",
+                rating: record.enjoyment,
             });
         });
     });
