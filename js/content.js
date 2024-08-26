b432909ccc5c3629a824394a5f04ec5d4422130d
sphericle <86196223+sphericle@users.noreply.github.com>
2024-08-25 22:07:59 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -164,7 +164,7 @@ export async function fetchLeaderboard() {
                     level: level.name,
                     score: score(level.difficulty, 100, level.percentToQualify),
                     link: record.link,
-                    rating: record.rating,
+                    rating: record.enjoyment,
                 });
                 return;
             }
