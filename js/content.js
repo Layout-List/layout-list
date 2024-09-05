0aaf56ed80af64ce0b8c0a0724f4381919f96e80
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-09-04 23:46:59 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -149,10 +149,10 @@ export async function fetchLeaderboard() {
         });
 
         // Creators
-        level.creators.forEach((creator) => {
+        level.creators.forEach((person) => {
             const creator = Object.keys(scoreMap).find(
-                (u) => u.toLowerCase() === creator.toLowerCase(),
-            ) || creator;
+                (u) => u.toLowerCase() === person.toLowerCase(),
+            ) || person;
             scoreMap[creator] ??= {
                 authored: [],
                 created: [],
