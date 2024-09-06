35786b8fe5c4e30e64572733b73fd8e2bf6a634f
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-09-05 23:14:03 -0400

    Update util.js

--- js/util.js
+++ js/util.js
@@ -10,7 +10,7 @@ export function embed(video) {
 }
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 1 });
+    return num.toLocaleString(undefined, { minimumFractionDigits: 2 });
 }
 
 export function getThumbnailFromId(id) {
