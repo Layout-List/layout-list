14dcc0ce28a92198a3f5c9207b318a2ad2ae7e77
sphericle <jackb2033@gmail.com>
2024-10-09 20:37:39 -0400

    let. me. cook!

--- js/pages/Packs.js
+++ js/pages/Packs.js
@@ -1,17 +1,17 @@
-import { store } from '../main.js';
-import { embed, rgbaBind } from '../util.js';
-import { score, round, averageEnjoyment } from '../score.js';
-import { fetchEditors, fetchList } from '../content.js';
+import { store } from "../main.js";
+import { embed } from "../util.js";
+import { score } from "../score.js";
+import { fetchEditors, fetchList } from "../content.js";
 
-import Spinner from '../components/Spinner.js';
-import LevelAuthors from '../components/List/LevelAuthors.js';
+import Spinner from "../components/Spinner.js";
+import LevelAuthors from "../components/List/LevelAuthors.js";
 
 const roleIconMap = {
-    owner: 'crown',
-    admin: 'user-gear',
-    helper: 'user-shield',
-    dev: 'code',
-    trial: 'user-lock',
+    owner: "crown",
+    admin: "user-gear",
+    helper: "user-shield",
+    dev: "code",
+    trial: "user-lock",
 };
 
 export default {
@@ -22,28 +22,22 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container">
-                <table class="list" v-if="list">
-                    <tr v-for="([err, rank, level], i) in list">
-                        <td class="rank">
-                            <p v-if="rank === null" class="type-label-lg">&mdash;</p>
-                            <p v-else class="type-label-lg">#{{ rank }}</p>
-                        </td>
-                        <td class="level" :class="{ 'active': selected == i, 'error': err !== null }">
-                            <button @click="selected = i">
-                                <span class="type-label-lg">{{ level?.name || 'Error (' + err + '.json)' }}</span>
+                <table class="list" v-if="packs">
+                    <tr v-for="(pack, index) in packs" :key="index">
+                        <td class="level" :class="{ 'active': selected == index, 'error': !pack }">
+                            <button @click="selected = index">
+                                <span class="type-label-lg">{{ pack.name }}</span>
                             </button>
                         </td>
                     </tr>
                 </table>
             </div>
             <div class="level-container">
-                <div class="level" v-if="level && level.id!=0">
-                    <h1>{{ level.name }}</h1>
-                    <div class="pack" :style="{ 'background': store.dark ? rgbaBind(level.packs.dark) : rgbaBind(level.packs.light) }" v-if="level.packs !== undefined">{{ level.packs.name }}</div>
-                </div>
-                <div v-else-if="level.id==0" class="level" style="height: 100%; justify-content: center; align-items: center;">
-                    <h1>{{ level.name }}</h1>
-                    <p>The levels below are {{ level.name.replace("(", "").replace(")", "") }}.</p>
+                <div class="level" v-if="selectedPack">
+                    <h1>{{ selectedPack.name }}</h1>
+                    <ul>
+                        <h3 v-for="level in selectedPack.levels" :key="level">{{ level }}</h3>
+                    </ul>
                 </div>
                 <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
@@ -52,63 +46,75 @@ export default {
             <div class="meta-container">
                 <div class="meta">
                     <div class="errors" v-show="errors.length > 0">
-                        <p class="error" v-for="error of errors">{{ error }}</p>
+                        <p class="error" v-for="error of errors" :key="error">{{ error }}</p>
                     </div>
                     <div class="og">
-                        <p class="type-label-md">Website layout on <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a>, made by DJ JDK & Blathers.</p>
+                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
                     </div>
                     <template v-if="editors">
-                        <h3>LIST EDITORS</h3>
+                        <h3>List Editors</h3>
                         <ol class="editors">
-                            <li v-for="editor in editors">
-                                <img :src="'/assets/' + roleIconMap[editor.role] + (store.dark ? '-dark' : '') + '.svg'" :alt="editor.role">
+                            <li v-for="editor in editors" :key="editor.name">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
                                 <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
                                 <p v-else>{{ editor.name }}</p>
                             </li>
                         </ol>
                     </template>
+                    <h3>Submission Requirements</h3>
+                    <p>
+                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                    </p>
+                    <p>
+                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                    </p>
+                    <p>
+                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                    </p>
+                    <p>
+                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                    </p>
+                    <p>
+                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                    </p>
+                    <p>
+                        Do not use secret routes or bug routes
+                    </p>
+                    <p>
+                        Do not use easy modes, only a record of the unmodified level qualifies
+                    </p>
+                    <p>
+                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                    </p>
                 </div>
             </div>
         </main>
     `,
     data: () => ({
         list: [],
+        packs: [],
         editors: [],
         loading: true,
-        selected: 1,
+        selected: 0,
         errors: [],
-        listlevels: 0,
         roleIconMap,
-        store,
-        toggledShowcase: false,
+        store
     }),
-  
     computed: {
-        level() {
-            return this.list && this.list[this.selected] && this.list[this.selected][2];
+        selectedPack() {
+            return this.packs[this.selected] || null;
         },
-        video() {
-            if (!this.level.showcase) {
-                return embed(this.level.verification);
-            }
-    
-            return embed(
-                this.toggledShowcase
-                    ? this.level.showcase
-                    : this.level.verification
-            );
-        },
-},
+    },
     async mounted() {
         // Hide loading spinner
-        this.list = await fetchList();            
+        this.list = await fetchList();
         this.editors = await fetchEditors();
-        
-        
+        this.packs = await this.getPacks(this.list);
+
         // Error handling
         if (!this.list) {
             this.errors = [
-                'Failed to load list. Retry in a few minutes or notify list staff.',
+                "Failed to load list. Retry in a few minutes or notify list staff.",
             ];
         } else {
             this.errors.push(
@@ -122,15 +128,37 @@ export default {
                 this.errors.push('Failed to load list editors.');
             }
         }
-            
+
         this.loading = false;
     },
-    
     methods: {
         embed,
         score,
-        averageEnjoyment,
-        rgbaBind
-},
+        async getPacks(list) {
+            // Collect packs and their respective levels
+            const packsMap = {};
+            let packs = [];
+            list.forEach((object) => { // list is an array > array with length of 3 > usually null (probably errors if any), level rank, level object
+                let level = object[2]; // why
 
+                if (level.packs) { // if the level is in a pack
+
+                    if (!packs[level.packs]) {
+
+                        packsMap[level.packs] = {
+                            name: level.packs.name,
+                            light: level.packs.light,
+                            dark: level.packs.dark,
+                            levels: [level.packs.levels],
+                        };
+
+                        packs.push(packsMap[level.packs]);
+                }
+            }
+            });
+            console.log(packs)
+
+            return packs;
+        },
+    },
 };
