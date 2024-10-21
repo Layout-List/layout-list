fac42f0589e232b3bb54dd7f0abe6a8e029fc078
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-10-21 00:10:21 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -40,7 +40,7 @@ export default {
                 <div class="level" v-if="level && level.id!=0">
                     <h1>{{ level.name }}</h1>
                     <div class="pack-container" v-if="level.packs.length > 1 || level.packs.length !== 0 && !level.packs[0]?.targetdiff">
-                        <div class="pack" v-for="pack in level.packs" :style="{ 'background': store.dark ? rgbaBind(pack.dark, 0) : rgbaBind(pack.light, 0), 'display': pack.targetdiff ? 'none' : 'inherit' }">{{ pack.name }}</div>
+                        <div class="pack" v-for="pack in level.packs" :style="{ 'background': store.dark ? rgbaBind(pack.dark, 0.2) : rgbaBind(pack.light, 0.2), 'display': pack.targetdiff ? 'none' : 'inherit' }">{{ pack.name }}</div>
                     </div>
                     <LevelAuthors :author="level.author" :hosts="level.hosts" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
                     <h3>Difficulty: {{["Beginner", "Easy", "Medium", "Hard", "Insane", "Mythical", "Extreme", "Supreme", "Ethereal", "Legendary", "Silent", "Impossible"][level.difficulty]}} layout</h3>
