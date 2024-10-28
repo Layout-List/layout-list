bc66fe8893c45d4db7f973e4862f565425d9f55c
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-10-28 03:04:36 -0400

    Update content.js

--- js/content.js
+++ js/content.js
@@ -138,9 +138,9 @@ export async function fetchChallengeList() {
 
 export async function fetchEditors() {
     try {
-        const editorsResults = await fetch(`${dir}/_editors.json`);
-        const editors = await editorsResults.json();
-        return editors;
+        const staffResults = await fetch(`${dir}/_staff.json`);
+        const staff = await staffResults.json();
+        return staff;
     } catch {
         return null;
     }
