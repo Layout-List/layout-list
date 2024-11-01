22710873fff9fdc5a90d32dd40073389659eef08
sphericle <jackb2033@gmail.com>
2024-10-31 21:33:09 -0400

    lmaorw

--- js/content.js
+++ js/content.js
@@ -310,6 +310,22 @@ export async function fetchLeaderboard(list) {
 
     packs.forEach((pack) => possibleMax += packScore(pack, list));
 
+    
+    packs.sort(
+        (x, y) => 
+            x.difficulty == 7 && // if the pack has difficulty 7 and is a difficulty pack
+            !x.levels ? 
+                -1 : // move the pack to the front of packs[]
+
+            y.difficulty == 7 ? // other packs in diff 7 are set below the diff pack
+                1 : 0
+            );
+
+    packs.sort(
+        (a, b) => 
+            b.difficulty - a.difficulty); // sort packs by difficulty!
+
+
     Object.entries(completedPacksMap).forEach(([user, packs]) => {
         const uniquePacks = Array.from(packs);
         scoreMap[user].userPacks.push(...uniquePacks);
@@ -460,7 +476,7 @@ export async function fetchPacks(list) {
 
     // manually override the order of the legendary packs
     // https://stackoverflow.com/a/23921775
-    
+
     packs.sort(
         (x, y) => 
             x.difficulty == 7 && // if the pack has difficulty 7 and is a difficulty pack
