5910ae93ec62015c2c4915b0a238bb6912d63d77
sphericle <86196223+sphericle@users.noreply.github.com>
2024-10-31 22:22:45 -0400

    try

--- js/content.js
+++ js/content.js
@@ -187,7 +187,11 @@ export async function fetchLeaderboard(list) {
                     const completedInDifficulty = list.filter(([_, __, lvl]) =>
                         lvl.difficulty === level.difficulty && 
                         lvl.verifier.toLowerCase() === verifier.toLowerCase() &&
-                        !lvl.packs.every((p) => p.levels?.includes(level))
+                        packs.some(
+                            (otherPack) =>
+                                otherPack.levels && 
+                                !otherPack.levels.some((otherLevel) => otherLevel.path === lvl.path)
+                        )
                     )
                     .length;
 
@@ -263,11 +267,11 @@ export async function fetchLeaderboard(list) {
                             // Count levels completed by the user in the current difficulty
                             const completedInDifficulty = list.filter(([_, __, lvl]) =>
                                 lvl.difficulty === level.difficulty && 
-                                lvl.records.some((r) => r.user === record.user && r.percent === 100 || lvl.verifier.toLowerCase() === user.toLowerCase()) &&
+                               lvl.records.some((r) => r.user === record.user && r.percent === 100 || lvl.verifier.toLowerCase() === user.toLowerCase()) &&
                                 packs.some(
                                     (otherPack) =>
                                         otherPack.levels && 
-                                        otherPack.levels.some((otherLevel) => otherLevel.path === lvl.path)
+                                        !otherPack.levels.some((otherLevel) => otherLevel.path === lvl.path)
                                 )
                             )
                             .length;
