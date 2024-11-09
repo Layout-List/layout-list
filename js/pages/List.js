012ebf64c6428821819e7bc93ecd66612d9688a8
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-11-08 22:52:38 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -104,8 +104,7 @@ export default {
                 <div v-else-if="level.id==0" class="level" style="height: 100%; justify-content: center; align-items: center;">
                     <h1>{{ level.name }}</h1>
                     <h2># of levels in tier: {{ fetchTierLength(list, level.difficulty) }}</h2>
-                    <h2>Points in tier: {{ localize(fetchTotalScore(list, level.difficulty)) }}</h2>
-                    <h2></h2>
+                    <h2 style="padding-bottom:1rem">Points in tier: {{ localize(fetchTotalScore(list, level.difficulty)) }}</h2>
                     <table class="records">
                         <tr class="record">
                             <td><h3 class="tier-info tier-info-header">Highest enjoyment: </h3></td>
