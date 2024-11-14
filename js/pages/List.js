84f97d744f9ea88015815284847447dbd4b869f5
lMystikl <123086188+lMystikl@users.noreply.github.com>
2024-11-13 20:07:23 -0500

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -193,33 +193,35 @@ export default {
                     </div>
                     <hr width="100%" color = black size="4">
                     <h3>Difficulty Rankings</h3>
-                    <p>
-                        Impossible Layout = Top Extreme Demons (401 to 750 Points)
-                    </p>
-                    <p>
-                        Legendary Layout = Mid Extreme Demons (201 to 400 Points)
-                    </p>
-                    <p>
-                        Extreme Layout = Beginner Extreme Demons (101 to 200 Points)
-                    </p>
-                    <p>
-                        Mythical Layout = High Insane Demons (71 to 100 Points)
-                    </p>
-                    <p>
-                        Insane Layout = Insane Demons (41 to 70 Points)
-                    </p>
-                    <p>
-                        Hard Layout = Hard Demons (21 to 40 Points)
-                    </p>
-                    <p>
-                        Medium Layout = Medium Demons (11 to 20 Points)
-                    </p>
-                    <p>
-                        Easy Layout = Easy Demons (6 to 10 Points)
-                    </p>
-                    <p>
-                        Beginner Layout = Non Demons (1 to 5 Points)
-                    </p>
+                    <div class="difficulty-rankings">
+                        <p>
+                            Impossible Layout = Top Extreme Demons (401 to 750 Points)
+                        </p>
+                        <p>
+                            Legendary Layout = Mid Extreme Demons (201 to 400 Points)
+                        </p>
+                        <p>
+                            Extreme Layout = Beginner Extreme Demons (101 to 200 Points)
+                        </p>
+                        <p>
+                            Mythical Layout = High Insane Demons (71 to 100 Points)
+                        </p>
+                        <p>
+                            Insane Layout = Insane Demons (41 to 70 Points)
+                        </p>
+                        <p>
+                            Hard Layout = Hard Demons (21 to 40 Points)
+                        </p>
+                        <p>
+                            Medium Layout = Medium Demons (11 to 20 Points)
+                        </p>
+                        <p>
+                            Easy Layout = Easy Demons (6 to 10 Points)
+                        </p>
+                        <p>
+                            Beginner Layout = Non Demons (1 to 5 Points)
+                        </p>
+                    </div>
                     <hr width="100%" color = black size="4">
                     <p>
                         For your convenience, the Layout List caches the data for the list in your browser.
