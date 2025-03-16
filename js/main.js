503fcae1122be4362bb1318c9af76f95e8655d2f
sphericle <86196223+sphericle@users.noreply.github.com>
2025-03-15 20:42:29 -0400

    dont show wholelist pack

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ console.clear();
 // the cached data if we push changes that would conflict with the old data, 
 // to prevent showing a billion error messages.
 export const version = 3.2
-const debug = false;
+const debug = true;
 
 export let store;
 
