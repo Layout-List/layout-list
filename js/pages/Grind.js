619f2f8cda96de25df4db332d52a6de71a131227
sphericle <86196223+sphericle@users.noreply.github.com>
2025-03-20 22:56:10 -0400

    Update Grind.js

--- js/pages/Grind.js
+++ js/pages/Grind.js
@@ -231,9 +231,11 @@ export default {
                     return;
                     
                 if (this.typedValues[path].enjoyment < 0) {
+                    alert("less")
                     this.typedValues[path].enjoyment = 0;
                     }
                 if (this.typedValues[path].enjoyment > 10){
+                    alert("greater")
                     this.typedValues[path.enjoyment] = 10
                     }
             }
