a9d6e5672e06414a466e4cc269f6aeedc94e2ac7
sphericle <86196223+sphericle@users.noreply.github.com>
2025-03-20 21:42:34 -0400

    omgogmgomgmog

--- js/pages/Grind.js
+++ js/pages/Grind.js
@@ -96,14 +96,17 @@ export default {
                                                 <p>{{ averageEnjoyment(level.records) }}/10</p>
                                             </li>
                                         </ul>
-                                        <br>
+                                    </div>
+                                    <br v-if="hovered === i">
+                                    <div class="extra-stats-container" v-if="hovered === i">
                                         <ul class="extra-stats">
                                             <li>
                                                 <div class="type-title-sm">{{ level.songLink ? "NONG" : "Song" }}</div>
-                                                <p class="director" v-if="level.songLink"><a target="_blank" :href="getSongDownload(level.songLink)" >{{ level.song || 'Song missing, please alert a list mod!' }}</a></p>
+                                                <p class="director" v-if="level.songLink"><a target="_blank" :href="songDownload" >{{ level.song || 'Song missing, please alert a list mod!' }}</a></p>
                                                 <p v-else>{{ level.song || 'Song missing, please alert a list mod!' }}</p>
                                             </li>
                                         </ul>
+                                        <br>
                                     </div>
                                 </div>
                             </div>
