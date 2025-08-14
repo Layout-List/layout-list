a8a6b19ac81b2cacffec45666ce40ec77bd6b788
sphericle <86196223+sphericle@users.noreply.github.com>
2025-08-14 00:23:54 -0400

    update this

--- js/components/Sidebar/PacksInfo.js
+++ js/components/Sidebar/PacksInfo.js
@@ -2,9 +2,9 @@ export default {
     template: `
         <h3>About Packs</h3>
         <div class="right-text">
-            <p>Packs are sets of levels on the Layout List chosen by the staff team that share distinct commonalities and are within a close difficulty range (generally ± 1 difficulty tier).</p>
+            <p>Packs are sets of levels on the Layout List chosen by the staff team that share distinct commonalities and are within a close difficulty range (generally ± 1 difficulty tier, with the exception of very specific and/or joke packs).</p>
             <p>If you have a suggestion for a new pack, feel free to share it with the list team in #list-discussion in our Discord server!</p>
-            <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!  Furthermore, send a screenshot of your list profile in #list-support in our Discord server, and we will give you the roles for the packs you've completed!</p>
+            <p>If you beat all the levels in a pack, it gets displayed on your profile in the leaderboard!  Furthermore, you can send a screenshot of your list profile in #list-support in our Discord server, and we will give you the roles for the packs you've completed!</p>
         </div>
     `
 }
